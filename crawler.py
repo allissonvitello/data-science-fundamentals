284c27836d0c8b386ebf08c05f88c9425b24063d
Allisson Vitello Campina <allissonvitello@gmail.com>
2017-12-05 22:18:39 -0200

    Initial commit

---
+++ crawler.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 05 21:39:48 2017
+
+@author: allisson
+
+------Not finished yet.------
+"""
+
+url = "http://www4.tjrj.jus.br/consultaProcessoWebV2/consultaProc.do?v=2&FLAGNOME=&back=1&tipoConsulta=publica&numProcesso="
+
+number_process = string(input("Por gentileza, informe o número de processo que deseja capturar: "))
+
+print(number_process)
