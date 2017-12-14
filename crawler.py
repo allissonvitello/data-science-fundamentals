52779fb3f6be501c05ce97e960762bd5c449a4da
Allisson Vitello Campina <allissonvitello@gmail.com>
2017-12-14 14:03:24 -0200

    Updated crawler.py and created functions.py

--- crawler.py
+++ crawler.py
@@ -7,6 +7,8 @@ Created on Tue Dec 05 21:39:48 2017
 ------Not finished yet.------
 """
 
+import functions as fct
+
 url = "http://www4.tjrj.jus.br/consultaProcessoWebV2/consultaProc.do?v=2&FLAGNOME=&back=1&tipoConsulta=publica&numProcesso="
 
 number_process = str(raw_input("Por gentileza, informe o número de processo que deseja capturar: "))
